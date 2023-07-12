a7a23221179832ddfdada27d6ed3c735e897ae10
William Antoline <65105895+williamantoline@users.noreply.github.com>
2023-07-12 13:27:59 +0700

    add test api

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+class TestController
+{
+    public function test(): JsonResponse
+    {
+        return response()->json([
+            'status' => 'ok'
+        ]);
+    }
+}
