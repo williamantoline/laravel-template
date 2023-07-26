05fcd6722244f4c000347de1f9448f019e9faecc
William Antoline <65105895+williamantoline@users.noreply.github.com>
2023-07-26 20:36:46 +0700

    test db

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 
 class TestController
 {
     public function test(): JsonResponse
     {
+        $a = User::first();
         return response()->json([
+            'a' => $a?->id,
             'status' => 'okay staging'
         ]);
     }
